6b18ba0092b12b2c4595febba05e15898c61cd94
Claudio Menegotto <menegotto.claudio@gmail.com>
2023-02-21 14:53:25 +0100

    update

--- assets/script/firestoreConnection.js
+++ assets/script/firestoreConnection.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
-import { getFirestore, getDocFromCache  } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-firestore.js";
+import { getFirestore, getDocFromCache, doc  } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-firestore.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
